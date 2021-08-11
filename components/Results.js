e00f1c48eaf8db3652f41b80f23b4f39406fe122
semih <schelik@uwaterloo.ca>
2021-08-11 13:39:51 -0400

    built the thumbnail component

--- components/Results.js
+++ components/Results.js
@@ -1,10 +1,11 @@
 import React from 'react'
+import Thumbnail from './Thumbnail'
 
-function Results({ requests }) {
+function Results({ results }) {
     return (
         <div>
-            {requests.map( result => (
-                <Thumbnail />
+            {results.map( result => (
+                <Thumbnail key={result.id} result={result} />
             ))}
         </div>
     )
