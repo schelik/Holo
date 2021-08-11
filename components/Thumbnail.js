b5bb93e912468f873edd8ed2c7cecbd8a10d1a48
semih <schelik@uwaterloo.ca>
2021-08-11 14:05:09 -0400

    final touch

--- components/Thumbnail.js
+++ components/Thumbnail.js
@@ -1,14 +1,14 @@
 import Image from 'next/image'
 import React from 'react'
 import { ThumbUpIcon } from "@heroicons/react/outline"
-import { forwardRef } from 'react'
 
-const Thumbnail = forwardRef(({ result }, ref) => {
+
+const Thumbnail = ({ result }) => {
     const BASE_URL = "https://image.tmdb.org/t/p/original/";
 
 
     return ( 
-        <div ref={ref} className="group cursor-pointer transition duration-200 ease-in transform sm:hover:scale-105 hover:z-50">
+        <div className="group cursor-pointer transition duration-200 ease-in transform sm:hover:scale-105 hover:z-50">
             <Image
                 layout="responsive"
                 src={
@@ -31,6 +31,6 @@ const Thumbnail = forwardRef(({ result }, ref) => {
             </div>
         </div>
     )
-})
+}
 
 export default Thumbnail
