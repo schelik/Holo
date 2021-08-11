6f416bd68d78e697a4cbfc64837958c470cce6d0
semih <schelik@uwaterloo.ca>
2021-08-11 12:06:17 -0400

    backend api added

--- components/Nav.js
+++ components/Nav.js
@@ -1,9 +1,14 @@
 import React from 'react'
+import requests from '../utils/requests'
 
 function Nav() {
     return (
         <nav>
-           <div></div> 
+           <div className="flex px-10 sm:px-20 text-2xl">
+               {Object.entries(requests).map(([key,{ title, url }]) => (
+               <h2 key={key} className="cursor-pointer  transform hover:scale-125% hover:text-white active:text-red-500 ">{title}</h2>
+               ))}
+           </div> 
         </nav>
     )
 }
